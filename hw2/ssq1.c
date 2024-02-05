46fb0fce46565948e42a20f9b62e42054f3dd074
LukeJYK <809698350@qq.com>
2024-02-05 12:36:50 -0500

    upload

--- hw2/ssq1.c
+++ hw2/ssq1.c
@@ -58,12 +58,7 @@
     double service;                            /*   service times      */
     double interarrival;                       /*   interarrival times */
   } sum = {0.0, 0.0, 0.0};
-  double max_delay = 0;                        /* max delay time       */ 
-  int time = 400;
-  int service_num = 0;                         /* number of jobs at service node at time t */
-  double total_job = 0;                        /* number of total jobs */
-  double delay_job_num = 0;                    /* number of delayed jobs at node*/
-  
+
   fp = fopen(FILENAME, "r");
   if (fp == NULL) {
     fprintf(stderr, "Cannot open input file %s\n", FILENAME);
@@ -73,43 +68,24 @@
   while (!feof(fp)) {
     index++;
     arrival      = GetArrival(fp);
-    if (arrival < departure){
+    if (arrival < departure) 
       delay      = departure - arrival;        /* delay in queue    */
-      delay_job_num++;
-      if (delay > max_delay)
-      {
-        max_delay = delay;
-      }
-    }
-    else {
+    else 
       delay      = 0.0;                        /* no delay          */
-    }
-    
     service      = GetService(fp);
     wait         = delay + service;
     departure    = arrival + wait;             /* time of departure */
-    if (departure > time && arrival < time)
-    {
-      service_num ++;
-    }
     sum.delay   += delay;
     sum.wait    += wait;
     sum.service += service;
-    total_job ++;
   }
   sum.interarrival = arrival - START;
-  double utilization = sum.service / departure;
-  
+
   printf("\nfor %ld jobs\n", index);
   printf("   average interarrival time = %6.2f\n", sum.interarrival / index);
   printf("   average service time .... = %6.2f\n", sum.service / index);
   printf("   average delay ........... = %6.2f\n", sum.delay / index);
   printf("   average wait ............ = %6.2f\n", sum.wait / index);
-  printf("   max delay ............ = %6.2f\n", max_delay);
-  printf("   your input time t........ = %0.2d\n", time);
-  printf("   number of jobs in the service node at time = 400............ = %d\n", service_num);
-  printf("   proportion of jobs delayed.. =  %0.0f%%\n", (delay_job_num/total_job)*100);
-  printf("   The proportion of utilization............ = %0.0f%%\n", utilization*100);
 
   fclose(fp);
   return (0);
